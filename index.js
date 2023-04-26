f6c6f6c1e460a4ed9490facb04ad4a0cafde0b43
johanlizma <124108921+johanlizma@users.noreply.github.com>
2023-04-26 10:23:56 -0500

    Create playRound function

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-function getcomputerchoice(){
+function getComputerChoice(){
     let results=["rock","paper","scissors"];
     let op=Math.floor(Math.random()*3);
     return results[op];
 }
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection.toLowerCase()==="paper" && computerSelection.toLowerCase()==="rock")
+        return `You Win! ${playerSelection.toLowerCase()} beats ${computerSelection.toLowerCase()}`;
+    else if(playerSelection.toLowerCase()==="scissors" && computerSelection.toLowerCase()==="paper")
+     return `You Win! ${playerSelection.toLowerCase()} beats ${computerSelection.toLowerCase()}`;
+    else if(playerSelection.toLowerCase()==="rock" && computerSelection.toLowerCase()==="scissors")
+     return `You Win! ${playerSelection.toLowerCase()} beats ${computerSelection.toLowerCase()}`;
+    else if(playerSelection.toLowerCase()===computerSelection.toLowerCase())
+     return `It's a tie!`;
+    else
+        return `You Lose! ${computerSelection.toLowerCase()} beats ${playerSelection.toLowerCase()}`;
+}
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
