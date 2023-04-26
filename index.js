54417d00bd6a28ccd9cf2a4a818ec0310f73c24b
johanlizma <124108921+johanlizma@users.noreply.github.com>
2023-04-26 12:46:20 -0500

    Create game function

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ function playRound(playerSelection, computerSelection) {
     else
         return `You Lose! ${computerSelection.toLowerCase()} beats ${playerSelection.toLowerCase()}`;
 }
-   
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+function game(){
+    for(let i=0;i<5;i++){  
+        const playerSelection = prompt("What is your choice?");
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
